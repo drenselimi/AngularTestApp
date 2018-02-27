ca63516f1fea75e57df91e4b27d88aca98b6cb4b
Dren Selimi <selimi.dreni@gmail.com>
2018-02-27 14:11:16 +0100

    Update app-route.module.ts

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import {PeopleComponent} from './people/people.component';
 import {PeopleCreateComponent} from './people/people-create/people-create.component';
 import { PeopleListComponent } from './people/people-list/people-list.component';
-import { PeopleDetailComponent } from './people/people-detail/people-detail.component';
 import { PeopleEditComponent } from './people/people-edit/people-edit.component';
 
 const appRoutes: Routes = [
@@ -12,8 +11,7 @@ const appRoutes: Routes = [
   { path: 'people', component: PeopleComponent, children: [
       { path: '', component: PeopleListComponent },
       {path: 'create', component: PeopleCreateComponent},
-      {path: 'edit/:mac', component: PeopleEditComponent},
-      {path: 'detail/:mac', component: PeopleDetailComponent}
+      {path: 'edit/:mac', component: PeopleEditComponent}
     ]}
 ];
 
